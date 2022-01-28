6d51fb0cc51c856695cec422b45b25af68f3cef9
rafael.xolio <rafael.xolio@neoris.com>
2022-01-28 00:09:25 -0600

    Adding Numbers Class

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace CSharp.Exercises.App
             //Samples.Strings.Run();
             Samples.Loops.Run();
             //Exercises.Problems.Run();  
+
         }
     }
 }
---
+++ Samples/Numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Exercises.App.Samples
+{
+    public static class Numbers
+    {
+
+        public static void MathRound()
+        {
+            //Even numbers round to nearest down
+            Console.WriteLine(Math.Round(6.5));
+            //Odd numbers round to nearest up
+            Console.WriteLine(Math.Round(13.5));
+
+        }
+    }
+}
